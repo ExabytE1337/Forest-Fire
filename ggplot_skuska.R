3f7f05de31ce6566ca6472602f0affa14a2f9690
ExabytE1337 <43238331+ExabytE1337@users.noreply.github.com>
2018-10-23 17:36:56 +0200

    Add files via upload

--- ggplot_skuska.R
+++ ggplot_skuska.R
@@ -1,7 +1,10 @@
 library(ggplot2)
 lis <- c(0,123,150,90,64,30,25)
+burnin <- 20
+delta <- 30
 d <- data.frame(1:length(lis),lis)
 names(d) <- c("iteration","isl")
 head(d)
-a <- ggplot(d,aes(iteration,isl))+geom_line(size=1)+geom_point(size = 3)
+a <- ggplot(d,aes(iteration,isl))+geom_line(size=1)+geom_point(shape = 19,size = 3)+xlim(1,burnin)+geom_vline(xintercept = burnin,linetype = "dashed")
+a <- a + geom_hline(yintercept = delta, linetype = "dashed")
 a
